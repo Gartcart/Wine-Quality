7b00bd0aa272252de9a807d37ac38b3b26cae425
Gartcart <gmscott237@gmail.com>
2024-03-11 17:09:47 -0500

    Added dependencies

--- data_set.py
+++ data_set.py
@@ -4,8 +4,8 @@ from ucimlrepo import fetch_ucirepo
 wine_quality = fetch_ucirepo(id=186)
 
 # data (as pandas dataframes)
-X = wine_quality.data.features
-y = wine_quality.data.targets
+features = wine_quality.data.features
+targets = wine_quality.data.targets
 
 # metadata
 print(wine_quality.metadata)
