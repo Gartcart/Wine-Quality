bdf7e586c34a7ddc21098ac1af9d952b90669aee
Emily Kimbrell <eak0043@auburn.edu>
2024-03-11 17:45:12 -0500

    starting on naive_bayes_classified

---
+++ naive_bayes_classifier.py
@@ -0,0 +1,6 @@
+from data_set import *
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, classification_report
+import pandas as pd
+
