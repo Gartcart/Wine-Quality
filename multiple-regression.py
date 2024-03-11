95670ad119e1d692c162ed9aea8337e243994b4b
Gartcart <gmscott237@gmail.com>
2024-03-11 17:44:14 -0500

    Fixed features

---
+++ multiple-regression.py
@@ -0,0 +1,26 @@
+from data_set import *
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+# x = input
+# y = output
+# Split into training and testing sets
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+print("MSE: ", mse)
+
+plt.scatter(y_test, y_pred)
+plt.xlabel("Actual Quality")
+plt.ylabel("Predicted Quality")
+plt.title("Actual vs Predicted Quality")
+plt.show()
