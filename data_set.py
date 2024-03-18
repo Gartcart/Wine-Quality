228c621398826745bd9b4578663346bc0b266a15
Emily Kimbrell <eak0043@auburn.edu>
2024-03-18 16:24:42 -0500

    weird format

--- data_set.py
+++ data_set.py
@@ -8,12 +8,9 @@ wine_quality = fetch_ucirepo(id=186)
 X = wine_quality.data.features
 # Y = data targets
 y = wine_quality.data.targets
-<<<<<<< HEAD
-=======
 
 # metadata
 # print(wine_quality.metadata)
 
 # variable information
 # print(wine_quality.variables)
->>>>>>> 4c0786561c8ea86417a3e735b8c16daa61a5b0f0
